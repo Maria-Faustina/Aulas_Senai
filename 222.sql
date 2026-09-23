fccd8538568c66d831b5c87600336b58ebf297fa
Maria Clara Faustina <mariaclarafsp299@gmail.com>
2026-09-23 17:10:31 -0300

    Add files via upload

--- 222.sql
+++ 222.sql
@@ -32,7 +32,8 @@ CREATE TABLE IF NOT EXISTS aluno(
     CONSTRAINT fk_aluno_usuario FOREIGN KEY (usuario_id) references usuario(id),
     CONSTRAINT fk_aluno_personal FOREIGN KEY (personal_id) references personal(id),
     data_nascimento DATE NOT NULL,
-    sexo VARCHAR(9) CHECK(SEXO IN('MASCULINO','FEMININO','OUTRO')) NOT NULL,
+    sexo VARCHAR(9) CHECK(SEXO IN('masculino','feminino','outro')) NOT NULL,
+    nivel VARCHAR(13) CHECK(nivel in('iniciante','intermediario','avancado')),
     objetivo_principal VARCHAR(200) NULL,
     observacao VARCHAR(256) NULL,
     data_inicio DATE NULL,
@@ -55,12 +56,16 @@ CREATE TABLE IF NOT EXISTS treino(
 	criado_em TIMESTAMP,
     atualizado_em TIMESTAMP
 );
+INSERT aluno_id() VALUES ();
+SELECT * FROM aluno;
 CREATE TABLE IF NOT EXISTS grupo_muscular(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome_grupo VARCHAR(50) UNIQUE
 );
 CREATE TABLE IF NOT EXISTS equipamento(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    grupo_muscular_id BIGINT NOT NULL,
+    CONSTRAINT fkgrupo_muscular FOREIGN KEY(grupo_muscular_id) references grupo_muscular(id),
     nome_equipamento VARCHAR(50) UNIQUE
 );
 CREATE TABLE IF NOT EXISTS exercicio(
@@ -71,12 +76,14 @@ CREATE TABLE IF NOT EXISTS exercicio(
     CONSTRAINT fk_exe_grupoMuscular FOREIGN KEY(grupo_muscular_id) references grupo_muscular(id),
 	CONSTRAINT fk_exe_equipamento FOREIGN KEY(equipamento_id) references equipamento(id),
     nome_exercicio VARCHAR(150) NOT NULL,
+    nivel VARCHAR(13) CHECK(nivel in('facil','intermediario','dificil')),
     CONSTRAINT exe_criado_por FOREIGN Key (personal_id)references personal(id),
-    instrucao VARCHAR(50),
+    instrucao VARCHAR(250),
     ativo BOOLEAN DEFAULT TRUE,
     criado_em TIMESTAMP,
     atualizado_em TIMESTAMP
 );
+
 CREATE TABLE IF NOT EXISTS treino_dia(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     treino_id BIGINT NOT NULL,
@@ -103,15 +110,15 @@ CREATE TABLE IF NOT EXISTS treino_exercicio(
     atualizado_em TIMESTAMP
 );
 
-INSERT INTO  usuario (nome, email, senha_hash, telefone, criado_em) VALUES ('Marco Aurélio','diogo.professor96@gmail.com','$2a$12$D1CMGMKVsWKaNqvb69iliuLkJdpGJ4V0mudT0fjtDtK3316t6E7xK','92000735','2026/09/22'), 
-('Feliperson Antonio vitto navas','marcosouzavitto11@gmail.com','$2a$12$tcZ819.ChnvqQIZnD71c3OUWU3qEqD/RXkAXgENGOkcfTQx9KpDPu','199996767','2026/09/22'), 
-('Tom Halland colleman','tomholland@email','$2a$12$Co19J7GlVsGRFrRYgYQqHupafCyonBpEaZLKLIvDNA0exM2N9.Ziy','4188867775','2026/09/22');
+INSERT INTO  usuario (nome, email, senha_hash, telefone, criado_em) VALUES ('Marco Aurélio','diogo.professor96@gmail.com','$2a$12$D1CMGMKVsWKaNqvb69iliuLkJdpGJ4V0mudT0fjtDtK3316t6E7xK','92000735','2026-09-22'), 
+('Feliperson Antonio vitto navas','marcosouzavitto11@gmail.com','$2a$12$tcZ819.ChnvqQIZnD71c3OUWU3qEqD/RXkAXgENGOkcfTQx9KpDPu','199996767','2026-09-22'), 
+('Tom Halland colleman','tomholland@email','$2a$12$Co19J7GlVsGRFrRYgYQqHupafCyonBpEaZLKLIvDNA0exM2N9.Ziy','4188867775','2026-09-22');
 
-INSERT INTO personal (usuario_id, cref, especialidade,biografia, link_rede_social, criado_em) VALUES (1, '676767-G/PR','estetica corporal','não tem','MarcoAurelio_personal','2026/09/22');
+INSERT INTO personal (usuario_id, cref, especialidade,biografia, link_rede_social, criado_em) VALUES (1, '676767-G/PR','estetica corporal','não tem','MarcoAurelio_personal','2026-09-22');
 SELECT * FROM grupo_muscular;
 
-INSERT INTO aluno(personal_id,usuario_id, data_nascimento, sexo, objetivo_principal, observacao, criado_em, data_inicio, nivel) VALUES ('1','1','2009/10/19','masculino','shaipe','tendao etorado','2026/09/22','2026/09/22','iniciante'),
-('1','2','2000/03/30','feminino','não tem','não tem','2026/09/22','2026/09/22','intermediario');
+INSERT INTO aluno(personal_id,usuario_id, data_nascimento, sexo, objetivo_principal, observacao, criado_em, data_inicio, nivel) VALUES ('1','1','2009-10-19','feminino','Melhorar o Fisico','tendao estorado','2026-09-22','2026-09-22','iniciante'),
+('1','2','2000-03-30','masculino','não tem','não tem','2026-09-22','2026-09-22','intermediario');
 
 INSERT INTO grupo_muscular (nome_grupo) VALUES ('Peitoral'), 
 ('Dorsais'),
@@ -121,11 +128,28 @@ INSERT INTO grupo_muscular (nome_grupo) VALUES ('Peitoral'),
 ('Abdômen');
 
 INSERT INTO equipamento (grupo_muscular_id, nome_equipamento) VALUES ('1','Banco de Supino'),
-('2','Puxada Alta'),
+('2','Máquina de Pulley'),
 ('3','Leg Press'),
 ('4','Elevação Lateral'),
 ('5','Máquina de Rosca Articulada'),
 ('6','Roda Abdominal');
-INSERT INTO exercicio (nivel) VALUES ('INICIANTE');
-INSERT criado_por
+
+INSERT exercicio (grupo_muscular_id, 
+equipamento_id, personal_id, nome_exercicio, nivel, instrucao,criado_em) VALUES 
+('1','1','1','Crucifixo com Halteres','intermediario','Deite no banco com halteres para cima. Abra os braços em arco largo, 
+flexionando levemente os cotovelos, e retorne contraindo o peito.','2026-09-11'),
+ 
+('2','2','1','Puxada Alta','facil','Sente na máquina com as coxas presas e segure a barra. Puxe em direção ao peito levando 
+os cotovelos para baixo, e retorne controlando o peso.','2026-09-01'),
+
+('5','5','1','Rosca Inversa','dificil','Sente na máquina com os braços apoiados, segure com palmas 
+para baixo e puxe o peso até os ombros. Retorne controlando.','2026-08-10');
+
+INSERT
+SELECT * from exercicio;
+
+
+
+
+
 
