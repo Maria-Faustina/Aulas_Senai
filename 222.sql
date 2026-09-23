959d5c711a5c29caccbe34d1193b943b6182e45e
Maria Clara Faustina <mariaclarafsp299@gmail.com>
2026-09-23 20:52:53 -0300

    Modify SQL schema and insert initial data
    Updated foreign key references and added new columns to various tables. Inserted new records into treino, treino_dia, and treino_exercicio tables.

--- 222.sql
+++ 222.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS treino(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     aluno_id BIGINT NOT NULL,
     personal_id BIGINT NOT NULL,
-    CONSTRAINT fk_treino_aluno FOREIGN KEY(aluno_id) references usuario(id),
+    CONSTRAINT fk_treino_aluno FOREIGN KEY(aluno_id) references aluno(id),
     CONSTRAINT fk_treino_personal FOREIGN KEY(personal_id) references personal(id),
     nome_treino VARCHAR(150) NOT NULL,
     descricao VARCHAR(250) NULL,
@@ -56,8 +56,7 @@ CREATE TABLE IF NOT EXISTS treino(
 	criado_em TIMESTAMP,
     atualizado_em TIMESTAMP
 );
-INSERT aluno_id() VALUES ();
-SELECT * FROM aluno;
+
 CREATE TABLE IF NOT EXISTS grupo_muscular(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     nome_grupo VARCHAR(50) UNIQUE
@@ -65,7 +64,7 @@ CREATE TABLE IF NOT EXISTS grupo_muscular(
 CREATE TABLE IF NOT EXISTS equipamento(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     grupo_muscular_id BIGINT NOT NULL,
-    CONSTRAINT fkgrupo_muscular FOREIGN KEY(grupo_muscular_id) references grupo_muscular(id),
+    CONSTRAINT fk_grupo_muscular FOREIGN KEY(grupo_muscular_id) references grupo_muscular(id),
     nome_equipamento VARCHAR(50) UNIQUE
 );
 CREATE TABLE IF NOT EXISTS exercicio(
@@ -90,25 +89,29 @@ CREATE TABLE IF NOT EXISTS treino_dia(
     exercicio_id BIGINT NOT NULL,
 	CONSTRAINT fk_treinoDia_treino FOREIGN KEY (treino_id) references treino(id),
     CONSTRAINT fk_treinoDia_exercicio FOREIGN KEY (exercicio_id) references exercicio(id),
+    nome VARCHAR(100) NOT NULL,
     ordem_treino INT NOT NULL,
-    intervalo INT NULL,
-    criado_em TIMESTAMP,
-    atualizado_em TIMESTAMP
+    criado_em TIMESTAMP
     
 );
+
 CREATE TABLE IF NOT EXISTS treino_exercicio(
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
     treino_dia_id BIGINT NOT NULL,
     exercicio_id BIGINT NOT NULL,
     CONSTRAINT fk_treinoExercicio_treinoDia FOREIGN KEY (treino_dia_id) references treino_dia(id),
     CONSTRAINT fk_treinoExercicio_exercicio FOREIGN KEY (exercicio_id) references exercicio(id),
-    ordem INT NOT NULL,
+    ordem INT NOT NULL DEFAULT 1,
+    carga_sugerida DECIMAL(8,2),
+    unidade_carga VARCHAR(13) CHECK(unidade_carga IN ('kg', 'lb', 'peso_corporal')),
+	descanso INT,
     repeticao INT NOT NULL,
     repeticao_max INT NOT NULL,
     observacao VARCHAR(250) NULL,
     criado_em TIMESTAMP,
     atualizado_em TIMESTAMP
 );
+
 
 INSERT INTO  usuario (nome, email, senha_hash, telefone, criado_em) VALUES ('Marco Aurélio','diogo.professor96@gmail.com','$2a$12$D1CMGMKVsWKaNqvb69iliuLkJdpGJ4V0mudT0fjtDtK3316t6E7xK','92000735','2026-09-22'), 
 ('Feliperson Antonio vitto navas','marcosouzavitto11@gmail.com','$2a$12$tcZ819.ChnvqQIZnD71c3OUWU3qEqD/RXkAXgENGOkcfTQx9KpDPu','199996767','2026-09-22'), 
@@ -117,8 +120,8 @@ INSERT INTO  usuario (nome, email, senha_hash, telefone, criado_em) VALUES ('Mar
 INSERT INTO personal (usuario_id, cref, especialidade,biografia, link_rede_social, criado_em) VALUES (1, '676767-G/PR','estetica corporal','não tem','MarcoAurelio_personal','2026-09-22');
 SELECT * FROM grupo_muscular;
 
-INSERT INTO aluno(personal_id,usuario_id, data_nascimento, sexo, objetivo_principal, observacao, criado_em, data_inicio, nivel) VALUES ('1','1','2009-10-19','feminino','Melhorar o Fisico','tendao estorado','2026-09-22','2026-09-22','iniciante'),
-('1','2','2000-03-30','masculino','não tem','não tem','2026-09-22','2026-09-22','intermediario');
+INSERT INTO aluno(personal_id,usuario_id, data_nascimento, sexo, objetivo_principal, observacao, criado_em, data_inicio, nivel) VALUES ('1','2','2009-10-19','feminino','Melhorar o Fisico','tendao estorado','2026-09-22','2026-09-22','iniciante'),
+('1','3','2000-03-30','masculino','não tem','não tem','2026-09-22','2026-09-22','intermediario');
 
 INSERT INTO grupo_muscular (nome_grupo) VALUES ('Peitoral'), 
 ('Dorsais'),
@@ -145,8 +148,25 @@ os cotovelos para baixo, e retorne controlando o peso.','2026-09-01'),
 ('5','5','1','Rosca Inversa','dificil','Sente na máquina com os braços apoiados, segure com palmas 
 para baixo e puxe o peso até os ombros. Retorne controlando.','2026-08-10');
 
-INSERT
-SELECT * from exercicio;
+INSERT treino (aluno_id, personal_id, nome_treino, descricao, objetivo, data_inicio, data_fim, criado_em) 
+VALUES (1,1,'Treino Hipertrofia A/B','acompanhamento profissional para ganho de
+ massa muscular','Ganhar mais musculos',
+'2026-09-22','2026-12-22','2026-09-22');
+INSERT treino (aluno_id, personal_id, nome_treino, descricao, objetivo, data_inicio, data_fim, criado_em) 
+VALUES (1,1,'Treino Hipertrofia A/B','acompanhamento profissional para ganho de massa muscular','Ganhar mais musculos',
+'2026-09-22','2026-12-22','2026-09-22');
+
+INSERT treino_dia (treino_id, exercicio_id, ordem_treino, nome, criado_em) VALUES 
+('1','1','1','Treino A - Peito e Tríceps','2026-09-22'),
+('1','3','2','Treino B - Pernas e Ombros','2026-09-23');
+
+INSERT treino_exercicio (treino_dia_id, 
+    exercicio_id, carga_sugerida, unidade_carga, repeticao, repeticao_max, descanso , observacao, criado_em) 
+    VALUES (1, 3, 3.0, 'kg', 10 ,12 ,1,'Sem observaçao','2026-10-22 20:30:00'),
+		   (1, 2, 15.0, 'kg', 10 ,12 ,1,'Sem observaçao','2026-10-22 20:30:00'),
+           (2, 1, 30.0, 'kg', 10 ,12 ,1,'Sem observaçao','2026-10-02 20:43:00'),
+           (2, 2, 30.0, 'kg', 5 ,10 ,1,'Sem observaçao','2026-10-02 20:43:00');
+
 
 
 
